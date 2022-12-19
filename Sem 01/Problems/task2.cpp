72ee30f8d6bdc6118028325c77066bf1bd2aee2b
Sider Ivanov <sider.ivanov@egt-interactive.local>
2022-12-19 21:51:23 +0200

    Add Sem 04 w problems

--- Sem 01/Problems/task2.cpp
+++ Sem 01/Problems/task2.cpp
@@ -5,7 +5,17 @@ int main()
 {
 	int n;
 	cin >> n;
+		
+	int thirdDigit = n % 10;
+	n /= 10; //remove the lastDigit;
 
-	cout << n / 10 << endl;
+	int secondDigit = n % 10;
+	n /= 10; //remove the lastDigit;
 
+	int firstDigit = n % 10;
+
+	int reversed = thirdDigit * 100 + secondDigit * 10 + firstDigit;
+	reversed++;
+
+	cout << reversed << endl;
 }
